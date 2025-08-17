70be9c08a0acc267a7e7bf1ded555451be41b10e
fobassojean7 <fobassojean7@gmail.com>
2025-08-17 00:58:12 -0400

    mod2 changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,10 +7,12 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-
-    #path for login
+    path('registration/', views.registration, name='registration'),
+    # path for login
     path(route='login', view=views.login_user, name='login'),
 
+    # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
     # path for dealer reviews view
 
     # path for add a review view
