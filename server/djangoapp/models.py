93aa7e3f174c412b15483a49494eef52e2207a9d
fobassojean7 <fobassojean7@gmail.com>
2025-08-19 06:42:51 -0400

    correction updtes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -7,6 +7,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
+
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
@@ -17,7 +18,7 @@ class CarMake(models.Model):
     description = models.TextField()
 
     def __str__(self):
-        return self.name #return the name as the string representation
+        return self.name  # return the name as the string representation
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -30,21 +31,21 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(
+        CarMake, on_delete=models.CASCADE
+    )  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
+        ("SEDAN", "Sedan"),
+        ("SUV", "SUV"),
+        ("WAGON", "Wagon"),
         # Add more choices as required
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
+    year = models.IntegerField(
+        default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)]
+    )
     # Other fields as needed
 
     def __str__(self):
-        return self.name  # Return the name as the string representation        
+        return self.name  # Return the name as the string representation
